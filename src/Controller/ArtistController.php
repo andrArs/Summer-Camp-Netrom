beb95f9e418519d7a6988a719491430e76fc097f
Andreea Arseanu <andreea.arseanu@student.upt.ro>
2025-06-29 20:08:39 +0300

    made some changes in database & added pagination for artists

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -3,25 +3,40 @@
 namespace App\Controller;
 
 use App\Repository\ArtistRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ArtistController extends AbstractController
 {
     public function __construct(private readonly ArtistRepository $artistRepository){}
-    #[Route('/artist', name: 'all_artists',methods: ['GET'])]
-    public function getAllArtists(): Response
+//    #[Route('/artist', name: 'all_artists',methods: ['GET'])]
+//    public function getAllArtists(): Response
+//    {
+//        $artists = $this->artistRepository->findAll();
+//
+//        return $this->render('artist/index.html.twig', [
+//            'artists' =>$artists
+//        ]);
+//}
+    #[Route('/artist', name: 'all_artists', methods: ['GET'])]
+    public function getAllArtists(Request $request, PaginatorInterface $paginator): Response
     {
-        $artists = $this->artistRepository->findAll();
+        $queryBuilder = $this->artistRepository->createQueryBuilder('a');
+
+
+        $pagination = $paginator->paginate(
+            $queryBuilder, //datele luate de mai sus din repo ul artist
+            $request->query->getInt('page', 1),
+            10 //nr de artisti pe pagina
+        );
 
         return $this->render('artist/index.html.twig', [
-            'artists' =>$artists
+            'pagination' => $pagination
         ]);
-
-
-
-}
+    }
 
     #[Route('/artist/{id}', name: 'one_artist',methods: ['GET'])]
     public function getOneArtist(int $id): Response
